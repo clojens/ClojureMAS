74aafa05e5138712bcf81d11400174d83d0f9fe8
fallen <fallen.s4e@gmail.com>
2013-02-26 10:01:44 +0600

    server finished

--- src/cabinet/web.clj
+++ src/cabinet/web.clj
@@ -8,13 +8,41 @@
    :headers {"Content-Type" "application/json"}
    :body (json/generate-string data)})
 
+(defn print-out[& xs]
+  (doall (map #(.print System/out %) xs))
+  (.println System/out ""))
+
+(defn reply-repl[expr]
+  (try
+    (print-out "expr = " expr)
+    (print-out "type of expr = " (type expr))
+    (print-out "read-str expr = " (read-string expr))
+    (print-out "type read-str expr = " (type (read-string expr)))
+    (print-out "evaled = " (eval (read-string expr)))
+    (print-out "type of evaled = " (type (eval (read-string expr))))
+    (str "(done " (eval (read-string expr)) ")")
+    (catch Exception e
+      (print-out "error occured : " e)
+      (str "(error \"" e "\")"))
+    (finally (print-out "----------------------"))))
+
 (defroutes handler
   (GET "/" []
-    (json-response {"hello" "world"}))
-
-  (PUT "/" [name]
-    (json-response {"hello" name})))
-
+       "try /repl/expr instead")
+  (GET "/repl/:expr" [expr]
+       (reply-repl expr)))
+  
 (def app
   (-> handler
-    wrap-json-params))
+      wrap-json-params))
+
+(comment
+(defroutes handler
+  (GET "/elems/:id" [id]
+       (json-response (elem/get id)))
+
+  (PUT "/elems/:id" [id attrs]
+       (json-response (elem/put id attrs)))
+
+  (DELETE "/elems/:id" [id]
+          (json-response (elem/delete id)))))
