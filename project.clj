34fd22d3eb3fb22fe499eee579ef9944577e3d11
fallen <fallen.s4e@gmail.com>
2013-02-26 11:52:34 +0600

    ws-client started & works

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-ws-client "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :main clojure-ws-client.core
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.1.3"]])
