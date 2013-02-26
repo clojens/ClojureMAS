e325f130b3f579876ece293d4086f4e661f90e3a
fallen <fallen.s4e@gmail.com>
2013-02-26 12:41:03 +0600

    printing is now prettier

--- src/cabinet/web.clj
+++ src/cabinet/web.clj
@@ -1,6 +1,7 @@
 (ns cabinet.web
   (:use compojure.core)
   (:use ring.middleware.json-params)
+  (:require ws.repl.utils)
   (:require [clj-json.core :as json]))
 
 (defn json-response [data & [status]]
@@ -14,16 +15,15 @@
 
 (defn reply-repl[expr]
   (try
-    (print-out "expr = " expr)
-    (print-out "type of expr = " (type expr))
-    (print-out "read-str expr = " (read-string expr))
-    (print-out "type read-str expr = " (type (read-string expr)))
-    (print-out "evaled = " (eval (read-string expr)))
-    (print-out "type of evaled = " (type (eval (read-string expr))))
-    (str "(done " (eval (read-string expr)) ")")
+    (print-out (str (java.util.Date.)))
+    (print-out "expr          = " expr)
+    (let [r (eval (read-string expr))]
+      (print-out "evaled value  = " r)
+      (print-out "evaled type   = " (type r))
+      (str "(:done " r ")"))
     (catch Exception e
-      (print-out "error occured : " e)
-      (str "(error \"" e "\")"))
+      (print-out "error occured = " e)
+      (str "(:error \"" e "\")"))
     (finally (print-out "----------------------"))))
 
 (defroutes handler
