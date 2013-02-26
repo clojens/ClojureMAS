d2f49123dfbcdc42a454ab8742c7085c3f1b21f8
fallen <fallen.s4e@gmail.com>
2013-02-26 23:13:47 +0600

    new simple repl added, catching errors added

--- src/main.clj
+++ src/main.clj
@@ -2,20 +2,21 @@
   (:use client.core)
   (:use ring.adapter.jetty)
   (:require service.web)
+  (:require clojure.main)
   (:gen-class))
+
+(def usage "Usage: app client | app server [port]")
 
 (defn main-client[ ]
   (println "Starting client")
   (println "Here is a simple repl. To enter remote repl type 'replr[& [addr]]', where addr = localhost:3000 by default")
-  (clojure.main/main))
+  (binding [*ns* (find-ns 'main)]
+    (client.core/repl)))
 
-(defn start-web []
-  )
-
-(defn main-server [& [addr]]
-  (println "Starting server"))
-
-(def usage "Usage: app client | app server [address]")
+(defn main-server [ [port] ]
+  (println "Starting server")
+  (run-jetty service.web/handler {:port (read-string (or port "3000"))
+                                  :join? false}))
 
 (defn -main [ & args ]
   (if (empty? args)
