05445b3fe5c35fea0f5dfb9cdc086c3270de8333
fallen <fallen.s4e@gmail.com>
2013-02-27 12:56:30 +0600

    temp

--- src/service/server.clj
+++ src/service/server.clj
@@ -4,6 +4,29 @@
   (:require service.web)
   (:gen-class))
 
-(defn start-server [ port ]
-  (println "Starting server")
-  (jetty/run-jetty service.web/handler {:port port :join? false}))
+(let [local-servers (atom {})] ; { port thread }
+  (defn start-server [ port ]
+    (when (= (@local-servers port) nil)
+      (let [server (atom nil)
+            thread (Thread.
+                    (fn[]
+                      (println "Starting server")
+                      (swap! server
+                             (fn[x]
+                               (jetty/run-jetty service.web/handler
+                                               {:port port :join? false})))))]
+        (swap! local-servers assoc port [server thread])
+        (.start thread)
+        )))
+
+  (defn shutdown-server [ port ]
+    "return nil if no such server found and Exception raised and returns t otherwise"
+    (let [ thread (@local-servers port) ]
+      (when thread
+        (try (do (.interrupt thread)
+                 (swap! local-servers dissoc port)
+                 true)
+             (catch Exception ex      nil)))))
+
+  (defn debug-local-servers[] local-servers))
+      
